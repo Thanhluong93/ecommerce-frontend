b990a795ad8b963f224b465c16bc1d75425417fa
Thanhluong93 <luong6011@gmail.com>
2025-04-10 08:13:08 +0700

    Thay giao dien

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,93 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { createRoot } from "react-dom/client"; // ✅ thêm dòng này
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = () => {
+import "./Navbar.css";
+
+export default function Header() {
+  useEffect(() => {
+    const hasChildLinks = document.querySelectorAll("li.has-child > a");
+
+    hasChildLinks.forEach((link) => {
+      const iconContainer = document.createElement("span");
+      const root = createRoot(iconContainer);
+      root.render(<FontAwesomeIcon icon={faChevronDown} style={{ marginLeft: "5px", fontSize: "12px" }} />);
+      link.appendChild(iconContainer);
+    });
+  }, []);
+
+
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">MyApp</div>
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-    </nav>
+    <div id="wrapper">
+      <div id="header">
+        <nav className="container">
+          <Link to="/Home" id="logo">
+            <img src="/images/logo.png" alt="Thanhluong.vn" />
+          </Link>
+          <ul id="main-menu">
+            <li>
+              <Link to="/">Trang Chủ</Link>
+            </li>
+            <li>
+              <Link to="/home">Đăng nhập</Link>
+              <ul className="submenu">
+                <li>
+                  <Link to="#">Menu 2.1</Link>
+                  <ul className="submenu">
+                    <li>
+                      <Link to="#">Menu 2.2.1</Link>
+                      <ul className="submenu">
+                        <li><Link to="#">Menu 2.2.1</Link></li>
+                        <li><Link to="#">Menu 2.2.2</Link></li>
+                        <li><Link to="#">Menu 2.2.3</Link></li>
+                        <li><Link to="#">Menu 2.2.4</Link></li>
+                      </ul>
+                    </li>
+                    <li><Link to="#">Menu 2.2.2</Link></li>
+                    <li>
+                      <Link to="#">Menu 2.2.3</Link>
+                      <ul className="submenu">
+                        <li><Link to="#">Menu 2.2.1</Link></li>
+                        <li><Link to="#">Menu 2.2.2</Link></li>
+                        <li><Link to="#">Menu 2.2.3</Link></li>
+                        <li><Link to="#">Menu 2.2.4</Link></li>
+                      </ul>
+                    </li>
+                    <li><Link to="#">Menu 2.2.4</Link></li>
+                  </ul>
+                </li>
+                <li><Link to="#">Menu 2.2</Link></li>
+                <li><Link to="#">Menu 2.3</Link></li>
+                <li><Link to="#">Menu 2.4</Link></li>
+              </ul>
+            </li>
+            <li><Link to="#">LAN</Link></li>
+            <li><Link to="#">Laptop</Link></li>
+            <li>
+              <Link to="#">CODE</Link>
+              <ul className="submenu">
+                <li><Link to="#">Menu 2.1</Link></li>
+                <li>
+                  <Link to="#">Menu 2.2</Link>
+                  <ul className="submenu">
+                    <li><Link to="#">Menu 2.2.1</Link></li>
+                    <li><Link to="#">Menu 2.2.2</Link></li>
+                    <li><Link to="#">Menu 2.2.3</Link></li>
+                    <li><Link to="#">Menu 2.2.4</Link></li>
+                  </ul>
+                </li>
+                <li><Link to="#">Menu 2.3</Link></li>
+                <li><Link to="#">Menu 2.4</Link></li>
+              </ul>
+            </li>
+            <li><Link to="/contact">Liên hệ</Link></li>
+            <li><Link to="/register">Đăng ký</Link></li>
+            <li><Link to="/login">Đăng nhập</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
-};
-
-export default Navbar;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,144 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home">
-      <h1>Welcome to Our Website</h1>
-      <p>This is the homepage of our amazing application.</p>
-      <div className="features">
-        <h2>Features</h2>
-        <ul>
-          <li>User-friendly interface</li>
-          <li>Responsive design</li>
-          <li>Easy navigation</li>
-        </ul>
+    <div className="container">
+    <div className="post-feature">
+      <a href="/" className="post-feature-media post-media">
+        <img
+          src="https://cdn.dribbble.com/userupload/17382372/file/original-97c91235cd1c453ed9e345f3a87863a2.jpg?resize=1024x768"
+          alt=""
+          className="post-feature-image"
+        />
+      </a>
+      <div className="post-feature-info">
+        <a href="/" className="post-category">The news</a>
+        <h2>
+          <a href="post_1.html" className="post-feature-title post-title">
+            Bitcoin trên đà lần đầu tiên cán mốc 90.000 USD
+          </a>
+        </h2>
+        <p className="post-desc">
+          Trong những ngày gần đây, thị trường tiền mã hóa đang trải qua cơn sốt
+          chưa từng có, đặc biệt là Bitcoin, khi đồng tiền này đã vượt qua mức giá
+          80.000 USD và hiện tại đang tiến gần đến ngưỡng 90.000 USD.
+        </p>
+        <a href="/" className="post-author">
+          <img
+            src="https://cdn.dribbble.com/userupload/17485458/file/original-14df872c516d361bb61de8ea029df5d8.png?resize=1024x1448"
+            alt=""
+            className="post-author-image"
+          />
+          <div className="post-author-infor">
+            <h4 className="post-author-name">By MrThanh</h4>
+            <time dateTime="" className="post-author-time">Just now</time>
+          </div>
+        </a>
       </div>
     </div>
+
+    <div className="post-list">
+      {/* Post 1 */}
+      <div className="post-item">
+        <a href="/" className="post-media">
+          <img
+            src="https://cdn.dribbble.com/userupload/17480453/file/original-3bec65ba8e096d26d5dbed25803dd983.jpg?resize=1024x1024"
+            alt=""
+            className="post-image"
+          />
+        </a>
+        <a href="/" className="post-category">Shop</a>
+        <h3>
+          <a href="/" className="post-title">
+            How to choose the best bike for spring in Australia
+          </a>
+        </h3>
+        <p className="post-desc">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis,
+          voluptatem? Quaerat, itaque fugit voluptate, eveniet animi aliquam non
+          quisquam ipsum soluta nisi magni expedita inventore facilis maiores
+          repudiandae, ullam nihil!
+        </p>
+        <a href="/" className="post-author">
+          <img
+            src="https://cdn.dribbble.com/userupload/17485458/file/original-14df872c516d361bb61de8ea029df5d8.png?resize=1024x1448"
+            alt=""
+            className="post-author-image"
+          />
+          <div className="post-author-infor">
+            <h4 className="post-author-name">By MRHELLO</h4>
+            <time dateTime="" className="post-author-time">13/2/2025</time>
+          </div>
+        </a>
+      </div>
+      {/* Post 2 */}
+      <div className="post-item">
+        <a href="/" className="post-media">
+          <img
+            src="https://cdn.dribbble.com/userupload/17487625/file/original-a1ef3696aa3b40521948adcfa442062c.png?resize=1024x804"
+            alt=""
+            className="post-image"
+          />
+        </a>
+        <a href="/" className="post-category">Shop</a>
+        <h3>
+          <a href="post.html?id=2" className="post-title">
+            How to choose the best bike for spring in Australia
+          </a>
+        </h3>
+        <p className="post-desc">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis,
+          voluptatem? Quaerat, itaque fugit voluptate, eveniet animi aliquam non
+          quisquam ipsum soluta nisi magni expedita inventore facilis maiores
+          repudiandae, ullam nihil!
+        </p>
+        <a href="/" className="post-author">
+          <img
+            src="https://cdn.dribbble.com/userupload/17485458/file/original-14df872c516d361bb61de8ea029df5d8.png?resize=1024x1448"
+            alt=""
+            className="post-author-image"
+          />
+          <div className="post-author-infor">
+            <h4 className="post-author-name">By MrThanh</h4>
+            <time dateTime="" className="post-author-time">Just now</time>
+          </div>
+        </a>
+      </div>
+      {/* Post 3 */}
+      <div className="post-item">
+        <a href="/" className="post-media">
+          <img
+            src="https://cdn.dribbble.com/userupload/17474398/file/original-636d2679cca02654092ec05b47ca1db0.png?resize=1024x770"
+            alt=""
+            className="post-image"
+          />
+        </a>
+        <a href="/" className="post-category">Shop</a>
+        <h3>
+          <a href="post.html?id=3" className="post-title">
+            How to choose the best bike for spring in Australia
+          </a>
+        </h3>
+        <p className="post-desc">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis,
+          voluptatem? Quaerat, itaque fugit voluptate, eveniet animi aliquam non
+          quisquam ipsum soluta nisi magni expedita inventore facilis maiores
+          repudiandae, ullam nihil!
+        </p>
+        <a href="/" className="post-author">
+          <img
+            src="https://cdn.dribbble.com/userupload/17485458/file/original-14df872c516d361bb61de8ea029df5d8.png?resize=1024x1448"
+            alt=""
+            className="post-author-image"
+          />
+          <div className="post-author-infor">
+            <h4 className="post-author-name">By MrThanh</h4>
+            <time dateTime="" className="post-author-time">Just now</time>
+          </div>
+        </a>
+      </div>
+      {/* Add more posts as necessary */}
+    </div>
+  </div>
   );
 };
 
