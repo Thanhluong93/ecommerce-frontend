2be358f6d903b88460c78ce5110f8aa9edd6f279
Thanhluong93 <luong6011@gmail.com>
2025-04-12 17:06:44 +0700

    Test

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000", // để test local
-    "https://ecommerce-frontend-indol-sigma.vercel.app/" // domain frontend trên Vercel của bạn
+    "https://ecommerce-frontend-indol-sigma.vercel.app" // domain frontend trên Vercel của bạn
   ]
 }));
 
